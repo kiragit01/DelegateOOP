3f75c100a9d5e1121a3ec54ca9836f4767c59f82
kiragit01 <kiragithub@gmail.com>
2024-05-19 17:18:48 +0300

    Добавьте файлы проекта.

---
+++ Operations.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateOOP
+{
+    public delegate void Operation(string message);
+    class Operations
+    {
+        Operation operation;
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Operations() : this(0,0) { } 
+        public Operations(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void AddOperation(Operation op)
+        {
+            operation += op;
+        }
+        public void DeleteOperation(Operation op)
+        {
+            operation -= op;
+        }
+
+        public void Sum()
+        {
+            operation.Invoke($"{X}+{Y}={X + Y}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateOOP
+{
+    class Program
+    {
+        static void Main()
+        {
+            Base();
+            Console.ReadKey();
+        }
+        
+        static void Base()
+        {
+            Console.Write("x= ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("y= ");
+            int y = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{x}+{y}={x+y}"); //а могло бы быть всё гораздо проще...
+            Operations operations = new Operations(x, y);
+            operations.AddOperation(Print);
+            operations.Sum();
+            operations.DeleteOperation(Print);
+        }
+
+        static void Print(string text) => Console.WriteLine(text);
+    }
+}
